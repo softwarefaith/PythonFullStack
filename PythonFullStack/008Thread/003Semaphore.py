b98fe1cc567671f2f346652cf15b5f7de099f577
蔡杰 <jie.cai@mljr.com>
2017-11-30 16:03:50 +0800

    相关理论

--- PythonFullStack/008Thread/003Semaphore.py
+++ PythonFullStack/008Thread/003Semaphore.py
@@ -1,5 +1,15 @@
 # 信号量
+"""
+同时只有n个线程可以获得semaphore,即可以限制最大连接数为n)
 
+Semaphore管理一个内置的计数器，
+每当调用acquire()时内置计数器-1；
+调用release() 时内置计数器+1；
+计数器不能小于0；
+当计数器为0时，acquire()将阻塞线程直到其他线程调用release()。
+
+
+"""
 import  threading, time
 
 
